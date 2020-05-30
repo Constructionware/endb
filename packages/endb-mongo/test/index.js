6d2951387361542e1f53f6b71526fcc45a467002
chroventer <ayyanlewis@gmail.com>
2020-05-30 10:30:23 -0700

    test(endb-mongo): add useUnifiedTopology option to URI

--- packages/endb-mongo/test/index.js
+++ packages/endb-mongo/test/index.js
@@ -4,7 +4,7 @@ const Endb = require('endb');
 const EndbMongo = require('../src');
 const { endbTest, adapterTest } = require('@endb/test');
 const { MONGO_HOST = '127.0.0.1' } = process.env;
-const uri = `mongodb://${MONGO_HOST}:27017`;
+const uri = `mongodb://${MONGO_HOST}:27017?useUnifiedTopology=true`;
 
 adapterTest(test, Endb, uri, 'mongodb://127.0.0.1:1234');
 endbTest(test, Endb, {
