a9c6e25953305ed665f6ed6a645f21f21654aa34
chroventer <ayyanlewis@gmail.com>
2020-06-03 11:31:15 -0700

    test: attempt to fix test errors

--- packages/endb-test/src/index.js
+++ packages/endb-test/src/index.js
@@ -1,141 +1,137 @@
 'use strict';
 
 module.exports.apiTest = (test, Endb, options = {}) => {
-  describe('API Test', () => {
-    beforeEach(async () => {
-      const endb = new Endb(options);
-      await endb.clear();
-    });
+  beforeEach(async () => {
+    const endb = new Endb(options);
+    await endb.clear();
+  });
 
-    test('All methods return a Promise.', () => {
-      const endb = new Endb(options);
-      expect(endb.all()).toBeInstanceOf(Promise);
-      expect(endb.clear()).toBeInstanceOf(Promise);
-      expect(endb.delete('foo')).toBeInstanceOf(Promise);
-      expect(endb.entries()).toBeInstanceOf(Promise);
-      expect(endb.get('foo')).toBeInstanceOf(Promise);
-      expect(endb.has('foo')).toBeInstanceOf(Promise);
-      expect(endb.keys()).toBeInstanceOf(Promise);
-      expect(endb.set('foo', 'bar')).toBeInstanceOf(Promise);
-      expect(endb.values() instanceof Promise);
-    });
+  test('All methods return a Promise.', () => {
+    const endb = new Endb(options);
+    expect(endb.all()).toBeInstanceOf(Promise);
+    expect(endb.clear()).toBeInstanceOf(Promise);
+    expect(endb.delete('foo')).toBeInstanceOf(Promise);
+    expect(endb.entries()).toBeInstanceOf(Promise);
+    expect(endb.get('foo')).toBeInstanceOf(Promise);
+    expect(endb.has('foo')).toBeInstanceOf(Promise);
+    expect(endb.keys()).toBeInstanceOf(Promise);
+    expect(endb.set('foo', 'bar')).toBeInstanceOf(Promise);
+    expect(endb.values() instanceof Promise);
+  });
 
-    test('.all() resolves to an array containing all the keys and values', async () => {
-      const endb = new Endb(options);
-      await endb.set('foo', 'bar');
-      expect(await endb.all()).toContainEqual({ key: 'foo', value: 'bar' });
-    });
+  test('.all() resolves to an array containing all the keys and values', async () => {
+    const endb = new Endb(options);
+    await endb.set('foo', 'bar');
+    expect(await endb.all()).toContainEqual({ key: 'foo', value: 'bar' });
+  });
 
-    test('.clear() resolves to undefined', async () => {
-      const endb = new Endb(options);
-      expect(await endb.clear()).toBeUndefined();
-      await endb.set('foo', 'bar');
-      expect(await endb.clear()).toBeUndefined();
-    });
+  test('.clear() resolves to undefined', async () => {
+    const endb = new Endb(options);
+    expect(await endb.clear()).toBeUndefined();
+    await endb.set('foo', 'bar');
+    expect(await endb.clear()).toBeUndefined();
+  });
 
-    test('.delete(key) resolves to true', async () => {
-      const endb = new Endb(options);
-      await endb.set('foo', 'bar');
-      expect(await endb.delete('foo')).toBe(true);
-    });
+  test('.delete(key) resolves to true', async () => {
+    const endb = new Endb(options);
+    await endb.set('foo', 'bar');
+    expect(await endb.delete('foo')).toBe(true);
+  });
 
-    test('.delete(key) with non-existent key resolves to false', async () => {
-      const endb = new Endb(options);
-      expect(await endb.delete('foo')).toBe(false);
-    });
+  test('.delete(key) with non-existent key resolves to false', async () => {
+    const endb = new Endb(options);
+    expect(await endb.delete('foo')).toBe(false);
+  });
 
-    test('.delete(key, path) deletes the property of the value', async () => {
-      const endb = new Endb(options);
-      const path = 'fizz.buzz';
-      await endb.set('foo', 'bar', path);
-      expect(await endb.delete('foo', path)).toBe(true);
-    });
+  test('.delete(key, path) deletes the property of the value', async () => {
+    const endb = new Endb(options);
+    const path = 'fizz.buzz';
+    await endb.set('foo', 'bar', path);
+    expect(await endb.delete('foo', path)).toBe(true);
+  });
 
-    test('.get(key) resolves to value', async () => {
-      const endb = new Endb(options);
-      await endb.set('foo', 'bar');
-      expect(await endb.get('foo')).toBe('bar');
-    });
+  test('.get(key) resolves to value', async () => {
+    const endb = new Endb(options);
+    await endb.set('foo', 'bar');
+    expect(await endb.get('foo')).toBe('bar');
+  });
 
-    test('.get(key) with non-existent key resolves to undefined', async () => {
-      const endb = new Endb(options);
-      expect(await endb.get('foo')).toBeUndefined();
-    });
+  test('.get(key) with non-existent key resolves to undefined', async () => {
+    const endb = new Endb(options);
+    expect(await endb.get('foo')).toBeUndefined();
+  });
 
-    test('.get(key, path) gets the property of the value', async () => {
-      const endb = new Endb(options);
-      const path = 'fizz.buzz';
-      await endb.set('foo', 'bar', path);
-      expect(await endb.get('foo', path)).toBe('bar');
-    });
+  test('.get(key, path) gets the property of the value', async () => {
+    const endb = new Endb(options);
+    const path = 'fizz.buzz';
+    await endb.set('foo', 'bar', path);
+    expect(await endb.get('foo', path)).toBe('bar');
+  });
 
-    test('.has(key) resolves to a true', async () => {
-      const endb = new Endb(options);
-      await endb.set('foo', 'bar');
-      expect(await endb.has('foo')).toBe(true);
-    });
+  test('.has(key) resolves to a true', async () => {
+    const endb = new Endb(options);
+    await endb.set('foo', 'bar');
+    expect(await endb.has('foo')).toBe(true);
+  });
 
-    test('.has(key) with non-existent key resolves to false', async () => {
-      const endb = new Endb(options);
-      expect(await endb.has('foo')).toBe(false);
-    });
+  test('.has(key) with non-existent key resolves to false', async () => {
+    const endb = new Endb(options);
+    expect(await endb.has('foo')).toBe(false);
+  });
 
-    test('.has(key, path) checks whether the property exists or not', async () => {
-      const endb = new Endb(options);
-      const path = 'fizz.buzz';
-      await endb.set('foo', 'bar', path);
-      expect(await endb.has('foo', path)).toBe(true);
-    });
+  test('.has(key, path) checks whether the property exists or not', async () => {
+    const endb = new Endb(options);
+    const path = 'fizz.buzz';
+    await endb.set('foo', 'bar', path);
+    expect(await endb.has('foo', path)).toBe(true);
+  });
 
-    test('.set(key, value) resolves to true', async () => {
-      const endb = new Endb(options);
-      expect(await endb.set('foo', 'bar')).toBe(true);
-    });
+  test('.set(key, value) resolves to true', async () => {
+    const endb = new Endb(options);
+    expect(await endb.set('foo', 'bar')).toBe(true);
+  });
 
-    test('.set(key, value) sets a value', async () => {
-      const endb = new Endb(options);
-      await endb.set('foo', 'bar');
-      expect(await endb.get('foo')).toBe('bar');
-    });
+  test('.set(key, value) sets a value', async () => {
+    const endb = new Endb(options);
+    await endb.set('foo', 'bar');
+    expect(await endb.get('foo')).toBe('bar');
+  });
 
-    test('.set(key, value, path) sets the property to the value', async () => {
-      const endb = new Endb(options);
-      const path = 'fizz.buzz';
-      await endb.set('foo', 'bar', path);
-      expect(await endb.get('foo')).toEqual({ fizz: { buzz: 'bar' } });
-    });
+  test('.set(key, value, path) sets the property to the value', async () => {
+    const endb = new Endb(options);
+    const path = 'fizz.buzz';
+    await endb.set('foo', 'bar', path);
+    expect(await endb.get('foo')).toEqual({ fizz: { buzz: 'bar' } });
+  });
 
-    afterEach(async () => {
-      const endb = new Endb(options);
-      await endb.clear();
-    });
+  afterEach(async () => {
+    const endb = new Endb(options);
+    await endb.clear();
   });
 };
 
 module.exports.adapterTest = (test, Endb, goodUri, badUri) => {
-  describe('Adapter Test', () => {
-    beforeEach(async () => {
-      const endb = new Endb(goodUri);
-      await endb.clear();
-    });
+  beforeEach(async () => {
+    const endb = new Endb(goodUri);
+    await endb.clear();
+  });
 
-    test('should infer the adapter from the URI', async () => {
-      const endb = new Endb(goodUri);
-      await endb.clear();
-      expect(await endb.get('foo')).toBeUndefined();
-      await endb.set('foo', 'bar');
-      expect(await endb.get('foo')).toBe('bar');
-      await endb.clear();
-    });
+  test('should infer the adapter from the URI', async () => {
+    const endb = new Endb(goodUri);
+    await endb.clear();
+    expect(await endb.get('foo')).toBeUndefined();
+    await endb.set('foo', 'bar');
+    expect(await endb.get('foo')).toBe('bar');
+    await endb.clear();
+  });
 
-    test('should emit connection errors', (done) => {
-      const endb = new Endb(badUri);
-      endb.on('error', () => done());
-    });
+  test('should emit connection errors', (done) => {
+    const endb = new Endb(badUri);
+    endb.on('error', () => done());
+  });
 
-    afterEach(async () => {
-      const endb = new Endb(goodUri);
-      await endb.clear();
-    });
+  afterEach(async () => {
+    const endb = new Endb(goodUri);
+    await endb.clear();
   });
 };
--- packages/endb/test/index.js
+++ packages/endb/test/index.js
@@ -2,34 +2,32 @@ const Endb = require('../src');
 const { apiTest } = require('@endb/test');
 const store = new Map();
 
-describe('Core', () => {
-  test('should be a class', () => {
-    expect(typeof Endb).toBe('function');
-    expect(() => Endb()).toThrow(); // eslint-disable-line new-cap
-    expect(() => new Endb()).not.toThrow();
-  });
+test('should be a class', () => {
+  expect(typeof Endb).toBe('function');
+  expect(() => Endb()).toThrow(); // eslint-disable-line new-cap
+  expect(() => new Endb()).not.toThrow();
+});
 
-  test('should integrate the adapter provided', async () => {
-    const store = new Map();
-    const endb = new Endb({ store });
-    expect(store.size).toBe(0);
-    await endb.set('foo', 'bar');
-    expect(await endb.get('foo')).toBe('bar');
-    expect(store.size).toBe(1);
-  });
+test('should integrate the adapter provided', async () => {
+  const store = new Map();
+  const endb = new Endb({ store });
+  expect(store.size).toBe(0);
+  await endb.set('foo', 'bar');
+  expect(await endb.get('foo')).toBe('bar');
+  expect(store.size).toBe(1);
+});
 
-  test('should integrate custom serializers provided', async () => {
-    const store = new Map();
-    const serialize = JSON.stringify;
-    const deserialize = JSON.parse;
-    const endb = new Endb({
-      store,
-      serialize,
-      deserialize,
-    });
-    expect(await endb.set('foo', 'bar')).toBe(true);
-    expect(await endb.get('foo')).toBe('bar');
+test('should integrate custom serializers provided', async () => {
+  const store = new Map();
+  const serialize = JSON.stringify;
+  const deserialize = JSON.parse;
+  const endb = new Endb({
+    store,
+    serialize,
+    deserialize,
   });
+  expect(await endb.set('foo', 'bar')).toBe(true);
+  expect(await endb.get('foo')).toBe('bar');
 });
 
 apiTest(test, Endb, { store });
